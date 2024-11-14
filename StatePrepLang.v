132621d531e2f71741678c8511c9e5eb80ee8b30
Anshu Sharma <anshu.g.sharma@gmail.com>
2024-11-14 10:07:25 -0500

    example in progress

--- StatePrepLang.v
+++ StatePrepLang.v
@@ -1,3 +1,19 @@
 Require Import OQASM.
+Require Import PQASM.
+Require Import BasicUtility.
 
-Inductive e  := H | CU posi e | OQASM.
+
+(*repeat until success: Inductive success or fail
+if fail -> repeat until success*)
+
+(*uniform superposition*)
+(*Ry Had (posi)*)
+Definition a: var :=3.
+Example (Ry (a,3)).
+
+(*permutations*)
+(*Fredkin gates; can be built with  *)
+
+(*Hamming weight*)
+
+(*Permutation*)
