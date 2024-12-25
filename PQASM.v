1a2d3a860cb796e6a41110a3f28c0d3291cea05c
Anshu Sharma <anshu.g.sharma@gmail.com>
2024-12-25 16:00:08 -0500

    in-progress work on amplitude amplification state

--- PQASM.v
+++ PQASM.v
@@ -553,8 +553,6 @@ Proof.
 
 Admitted.
 
-
-
 (* Testing Semantics. *)
 
 
@@ -765,6 +763,27 @@ End Hamming.
 (* Sample (choose (0,10)). *)
 QuickChick (Hamming.hamming_state_correct 100).
 
+Module AmplitudeAmplification.
+
+  (* Returns an expression to run P on each qubit position in reg*)
+  Fixpoint repeat (reg: list posi) (P: (posi -> exp)) :=
+    match reg with
+    | nil => ESKIP
+    | p::r => (P p) [;] (repeat r P) 
+    end.
+
+  Fixpoint pow_2 (n: nat) :=
+    match n with
+    | 0 => 1
+    | S m => 2*(pow_2 (m))
+    end.
+Check nat2fb.
+    Definition amplitude_amplification_state (n r:nat) (h_n:nat) (w:nat) :=
+    New (lst_posi n x_var) [;] New (lst_posi h_n y_var) [;] Had (lst_posi n x_var) [;]
+      repeat (lst_posi h_n y_var)  (fun (p:posi) => Next (Ry p (fun (n:nat)=> nat2fb n (r/pow_2 n)))).
+      
+End AmplitudeAmplification.
+
 Module SumState.
 
   (* Number of quantum registers *)
@@ -932,7 +951,7 @@ Module ModExpState.
       (lst_posi num_qubits x_var)
       num_qubits
       (fun (p: posi) (i: nat) => (
-        let A := (mod_pow c (pow 2 (i-1)) N (i+20)) in
+        let A := (mod_pow c (pow 2 (i-1)) N (i)) in
           (ICU p (Ora (ModMult (lst_posi num_exp_qubits y_var) (nat2fb A) (nat2fb N))))
       )
       )
@@ -950,136 +969,3 @@ Module ModExpState.
 End ModExpState.
 
 QuickChick (ModExpState.mod_exp_state_correct).
-
-(*
-Require Import Bvector.
-Require Import Testing.
-Require Import CLArith.
-From QuickChick Require Import QuickChick.
-
-Definition f_env := var -> nat.
-
-*)
-
-(* #[export] Instance shrinkExp : Shrink exp :=
-  {
-    shrink exp :=
-      match exp with
-      | ESKIP => [ ]
-      | Meas a b c => [ IFa a b c]
-      end
-  }. *)
-
-(*
-QuickChick (Test_prop.uniform_state_eskip_behavior).
-
-
-QuickChick (Test_prop.uniform_state_new_behavior).
-QuickChick (Test_prop.uniform_state_new_eskip_behavior).
-
-Add [q1,q2] 1
-
-take q1 and q2 value in st, and then compose them to be a bitstring, and then add 1, 
-,after adding one, then you resolve them into two bits.
-
-nat -> anything, use update
-
-posi -> anything, use st[p |-> up_h (st p)]
-
-1 / 2^rmax
-
-rz_val : nat -> bool, a bitstring, 0 is the max number, and rz_val (rmax - 1) is the least significant bit,
-
-0_position: 1/2. 
-1_position: 1/4.
-2_position: 1/8.
-
-i * 2 pi  3/2 pi == rz_val , 0 -> True, 1 -> true ,, (1/2 + 1/4) * 2 pi 
-
-*)
-
-(* This is the semantics for basic gate set of the language. 
-Fixpoint pexp_sem (env:var -> nat) (e:pexp) (st: posi -> val) : (posi -> val) :=
-   match e with (Oexp e') => (exp_sem env e' st)
-               | H p => st[p |-> up_h (st p)]
-              | e1 [;] e2 => pexp_sem env e2 (pexp_sem env e1 st)
-    end.
-
- (env:var -> nat) (i:iota) (st: posi -> val) : (posi -> val) :=
-
-*)
-
-(* Fixpoint instruction_sem (env:var -> nat) (i:iota) (st: posi -> val) : (posi -> val) :=
-match i with
-| Ry (p: posi) (r: Q) => match (st p) with
-    | =>
-    | => 
-    | _ =>
-| ICU (p: posi) (k: iota) => (instruction_sem env k st).
-| AddInst (k: iota) (m: iota) => (instruction_sem env k st)
-| mu (e:exp) (x:list posi) => (exp_sem exp). *)
-
-
-(* Inductive well_typed_pexp (aenv: var -> nat) : env -> pexp -> env -> Prop :=
-    | oexp_type : forall env e env', well_typed_oexp aenv env e env' -> well_typed_pexp aenv env (Oexp e) env'
-    | rz_had : forall env b p q, Env.MapsTo (fst p) (Had b) env -> well_typed_pexp aenv env (Oexp (RZ q p)) env
-    | rrz_had : forall env b p q, Env.MapsTo (fst p) (Had b) env -> well_typed_pexp aenv env (Oexp (RRZ q p)) env
-    | h_nor : forall env env' p, Env.MapsTo (fst p) Nor env -> snd p = 0 ->
-                  Env.Equal env' (Env.add (fst p) (Had 0) env) ->  
-                  well_typed_pexp aenv env (H p) env'
-    | h_had : forall env env' p b, Env.MapsTo (fst p) (Had b) env -> snd p = S b ->
-              Env.Equal env' (Env.add (fst p) (Had (S b)) env) ->  
-                  well_typed_pexp aenv env (H p) env'
-    | pe_seq : forall env env' env'' e1 e2, well_typed_pexp aenv env e1 env' -> 
-                 well_typed_pexp aenv env' e2 env'' -> well_typed_pexp aenv env (e1 [;] e2) env''. *)
-
-(* Inductive pexp_WF (aenv:var -> nat): pexp -> Prop :=
-     | oexp_WF : forall e, exp_WF aenv e -> pexp_WF aenv (Oexp e)
-     | h_wf : forall p, snd p < aenv (fst p) -> pexp_WF aenv (H p)
-      | pseq_wf : forall e1 e2, pexp_WF aenv e1 -> pexp_WF aenv  e2 -> pexp_WF aenv  (PSeq e1 e2). *)
-
-(*
-               | H p => st[p |-> up_h (st p)]
-
-      | h_fresh : forall p p1 , p <> p1 -> exp_fresh aenv p (H p1)
-
-      | h_neul : forall l y, exp_neu_l x l (H y) l
-
-    | rz_had : forall env b p q, Env.MapsTo (fst p) (Had b) env -> well_typed_exp AE env (RZ q p)
-    | rrz_had : forall env b p q, Env.MapsTo (fst p) (Had b) env -> well_typed_exp AE env (RRZ q p)
-
-              | H p => ((fst p)::[])
-
-    | h_nor : forall env env' p, Env.MapsTo (fst p) Nor env -> snd p = 0 ->
-                  Env.Equal env' (Env.add (fst p) (Had 0) env) ->  
-                  well_typed_pexp aenv env (H p) env'
-    | h_had : forall env env' p b, Env.MapsTo (fst p) (Had b) env -> snd p = S b ->
-              Env.Equal env' (Env.add (fst p) (Had (S b)) env) ->  
-                  well_typed_pexp aenv env (H p) env'
-
-      | h_wf : forall p, snd p < aenv (fst p) -> exp_WF aenv (H p).
-
-*)
-
-(*  Define approximate QFT in the Had basis by the extended type system. In this type system, 
-    once a value is in Had basis,
-     it will never be applied back to Nor domain, so that we can define more SR gates. *)
-(* Fixpoint many_CR (x:var) (b:nat) (n : nat) (i:nat) :=
-  match n with
-  | 0 | 1 => SKIP (x,n)
-  | S m  => if b <=? m then (many_CR x b m i ; (CU (x,m+i) (RZ n (x,i)))) else SKIP (x,m)
-  end. *)
-
-(* approximate QFT for reducing b ending qubits. *)
-(* Fixpoint appx_QFT (x:var) (b:nat) (n : nat) (size:nat) :=
-  match n with
-  | 0    => Oexp (SKIP (x,n))
-  | S m => if b <=? m then ((appx_QFT x b m size)) [;] (H (x,m))
-                    [;] (Oexp (many_CR x b (size-m) m)) else Oexp (SKIP (x,m))
-  end. *)
-
-(* Match the K graph by LV decomposition, the following define the L part.  (H ; R(alpha)); H  |x> -> Sigma|y>. *)
-(* Fixpoint half_k_graph (x:var) (r:nat) (size:nat) :=
-   match size with 0 => (Oexp (SKIP (x,0)))
-          | S m => (H (x,m)) [;] (Oexp (RZ r (x,m))) [;] half_k_graph x r m
-   end. *)
